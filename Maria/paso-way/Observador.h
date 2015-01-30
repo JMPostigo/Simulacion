78d2cf12ad7f97b66dca9632c4f6276cdf359100
Maria <chimichurri_cereza@live.com>
2015-01-30 13:31:56 +0100

    ** Subiendo ficheros a Simulacion/Maria/paso-way

---
+++ Maria/paso-way/Observador.h
@@ -0,0 +1,62 @@
+
+/* -*- Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+
+/*  PRÁCTICA 8. PLANIFICACIÓN Y SIMULACIÓN DE REDES
+*     Observador.h
+*       -Ramón Pérez Hernández
+*       -José Manuel Postigo Aguilar
+*       -María Valero Campaña
+*/
+
+#include "ns3/application.h"
+#include "ns3/packet.h"
+#include "ns3/address.h"
+#include "ns3/double.h"
+
+using namespace ns3;
+
+class Observador
+{
+public:
+
+  // Constructor de la clase.
+  Observador();
+
+  // Método para resetear los atributos del observador.
+  void Inicializa();
+
+  /* Método de gestión de la traza de envío completo de un paquete por la 
+  aplicación origen. */
+  void GestionaTrazaTxApp(Ptr <const Packet> p);
+
+  /* Método de gestión de la traza de recepción completa de un paquete en la
+  aplicación destino. */
+  void GestionaTrazaRxApp(Ptr <const Packet> p, const Address & direccion);
+
+  // Método que captura las trazas consideradas.
+  void CapturaTrazas(Ptr<Application> tx, Ptr<Application> rx);
+
+  // Método que devuelve el porcentaje de paquetes correctos en la aplicación destino.
+  double DevuelvePorcentajeCorrectos();
+
+  // Paquetes enviados por la aplicación origen.
+  double pkts_enviados;
+  
+  // Paquetes recibidos por la aplicación destino.
+  double pkts_recibidos;
+
+private:
+
+  // Aplicación transmisora en el escenario.
+  Ptr<Application> transmisor;
+
+  // Aplicación receptora en el escenario.
+  Ptr<Application> receptor;
+
+
+
+};
+
+
+
+
